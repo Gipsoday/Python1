07324589552155384cd3080f0b723e9dd6d58c77
Дмитрий Ящекно <144241340+Gipsoday@users.noreply.github.com>
2024-01-27 18:15:20 +0300

    Update lesson_1_task_7.py

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,43 +1,43 @@
 def num1(num):
-    print(num)
+    print(num, end=" ")
 num1(8) 
 
-def num2(num):
-    print(num)
+def num2(num,):
+    print(num, end = " ")
 num2(8) 
 
 def num3(num):
-    print(num)
+    print(num, end = " ")
 num3(0) 
 
 def num4(num):
-    print(num)
+    print(num, end = " ")
 num4(0) 
 
 def num5(num):
-    print(num)
+    print(num, end = " ")
 num5(5) 
 
 def num6(num):
-    print(num)
+    print(num, end = " ")
 num6(5) 
 
 def num7(num):
-    print(num)
+    print(num, end = " ")
 num7(5) 
 
 def num8(num):
-    print(num)
+    print(num, end = " ")
 num8(3) 
 
 def num9(num):
-    print(num)
+    print(num, end = " ")
 num9(5) 
 
 def num0(num):
-    print(num)
+    print(num, end = " ")
 num0(3) 
 
 def num0(num):
-    print(num)
+    print(num, end = " ")
 num0(5) 
